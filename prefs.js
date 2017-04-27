3f38d64cb833cbf9f0cfe0fea1880013f6d4c8d1
Stuart Langridge <sil@kryogenix.org>
2017-04-27 10:21:13 +0100

    Reformat prefs window, and add a few more examples

--- prefs.js
+++ prefs.js
@@ -11,6 +11,14 @@ const Convenience = Me.imports.convenience;
 function init() {
 }
 
+const EXAMPLES = [
+    ["The time as HH.MM", "%H.%M"],
+    ["The time in 24-hour notation (7:30:00 am BST)", "%r"],
+    ["A bell", "🔔"],
+    ["ISO date and time (2014-01-30T04:27:00)", "%FT%T"],
+    ["Something sillier", "It is %M minutes past hour %H"]
+];
+
 const ClockOverrideSettings = new GObject.Class({
     Name: 'ClockOverridePrefs',
     Extends: Gtk.Grid,
@@ -19,7 +27,7 @@ const ClockOverrideSettings = new GObject.Class({
 
         this.parent(params);
         this.margin = 24;
-        this.spacing = 30;
+        this.column_spacing = 30;
         this.row_spacing = 10;
         this._settings = Convenience.getSettings();
 
@@ -41,20 +49,62 @@ const ClockOverrideSettings = new GObject.Class({
                  this._settings.set_string('override-string', value);
              }
         }));
+        this.attach(label, 0, 0, 2, 1);
+        this.attach(widget, 2, 0, 1, 1);
+
         let label2 = new Gtk.Label({
-            label: 'Some examples:\n<b>ISO time</b>: <tt><a href="%FT%TZ">%FT%TZ</a></tt>\n<b>Nice picture of a clock</b>: <tt><a href="🕐">🕐</a></tt>',
+            label: 'Some examples:',
             use_markup: true,
             hexpand: true,
             halign: Gtk.Align.START
         });
-        label2.connect("activate-link", Lang.bind(this, function(w, uri) {
-            this._settings.set_string('override-string', uri);
-            return true;
-        }));
+        this.attach(label2, 0, 1, 3, 1);
 
-        this.attach(label, 0, 1, 1, 1);
-        this.attach(widget, 1, 1, 1, 1);
-        this.attach(label2, 0, 2, 2, 1);
+        var css = new Gtk.CssProvider();
+        css.load_from_data("GtkLabel { text-decoration: underline rgba(128, 128, 128, 0.3); }");
+
+        var grid = this;
+        var rownumber = 2;
+        EXAMPLES.forEach(function(eg) {
+            let l = new Gtk.Label({
+                label: '<b>' + eg[0] + '</b>:',
+                use_markup: true,
+                hexpand: true,
+                halign: Gtk.Align.START
+            });
+            /* It would be nicer to use just a Label with a hyperlink in it here, for accessibility
+               reasons, but god alone knows how to remove the underline on a label, thanks to the
+               lack of Gtk CSS examples. And a Button would be ugly. So an EventBox it is for now. */
+            let eb = new Gtk.EventBox({
+                hexpand: true,
+                halign: Gtk.Align.START
+            })
+            let r = new Gtk.Label({
+                label: '<tt>' + eg[1] + '</tt>',
+                use_markup: true,
+                hexpand: true,
+                halign: Gtk.Align.START
+            });
+            eb.add(r);
+            eb.connect("button-press-event", Lang.bind(grid, function(w) {
+                grid._settings.set_string('override-string', eg[1]);
+                return true;
+            }));
+            r.get_style_context().add_provider(css, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+            grid.attach(l, 0, rownumber, 1, 1);
+            grid.attach(eb, 1, rownumber, 2, 1);
+            rownumber += 1;
+        })
+
+        let label3 = new Gtk.Label({
+            label: '<a href="http://strftime.org/">What do all these % codes mean?</a>',
+            use_markup: true,
+            hexpand: true,
+            halign: Gtk.Align.END
+        });
+        this.attach(label3, 0, rownumber, 3, 1);
+
 
         this._settings.connect('changed::override-string', Lang.bind(this, function() {
             let sval = this._settings.get_string('override-string');
