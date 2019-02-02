7218039779a59f27617cce2ee7d780461b87d5c9
Unknown <brandl.matthaeus@gmail.com>
2019-02-02 08:00:49 +0100

    Make install-jasmine-gjs.sh use mktemp to obtain a temporary directory

--- test/install-jasmine-gjs.sh
+++ test/install-jasmine-gjs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -ex
 
-cd /tmp
+tempdir=`mktemp --directory --tmpdir install-jasmin.XXXXXXXXXX`
+cd $tempdir
 git clone https://github.com/ptomato/jasmine-gjs 
 cd jasmine-gjs
 ./autogen.sh
 make
 sudo make install
-
