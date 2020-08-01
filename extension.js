8d837d169d9636de6becb6b6e3e43f61ad7f19ae
Henrique Lechner <hlechner@gmail.com>
2020-08-01 15:42:05 -0300

    Keep the text align even with multi-line clock

--- extension.js
+++ extension.js
@@ -35,6 +35,8 @@ function enable() {
         print("Looks like Shell has changed where things live again; aborting.");
         return;
     }
+    
+    sA.dateMenu.actor.set_style("text-align: center;");
 
     sA.dateMenu.actor.first_child.get_children().forEach(function(w) {
         // assume that the text label is the first StLabel we find.
